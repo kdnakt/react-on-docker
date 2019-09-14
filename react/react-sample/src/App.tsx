0906b4d2ea27353a4d6530c9ba069b5916a734d9
kdnakt <a.kid.1985@gmail.com>
2019-09-14 22:45:19 +0900

    fix routes

--- react/react-sample/src/App.tsx
+++ react/react-sample/src/App.tsx
@@ -1,51 +1,24 @@
 import React from 'react';
 import {
-  Navbar, Nav, NavDropdown, Form, FormControl, Button,
+  Navbar,
+  Nav,
 } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import ReactRouterOnly from './page/ReactRouterOnly';
+import { Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (<>
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="/react-router-only">React Router</Nav.Link>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
     </Navbar>
 
-    <Navbar>
-      <Navbar.Toggle aria-controls="navbar-nav-pills" />
-      <Navbar.Collapse id="navbar-nav-pills">
-        <Nav className="flex-column" variant="pills" >
-          <LinkContainer to="#hoge0">Hoge0</LinkContainer>
-          <Nav.Link href="#hoge1">Hoge1</Nav.Link>
-          <Nav.Link href="#hoge2">Hoge2</Nav.Link>
-          <Nav.Link href="#hoge3">Hoge3</Nav.Link>
-          <Nav.Link href="#hoge4">Hoge4</Nav.Link>
-          <Nav.Link href="#hoge5">Hoge5</Nav.Link>
-          <Nav.Link>Hoge11</Nav.Link>
-          <Nav.Link>Hoge12</Nav.Link>
-          <Nav.Link>Hoge13</Nav.Link>
-          <Nav.Link>Hoge14</Nav.Link>
-          <Nav.Link>Hoge15</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <Route path="/react-router-only" component={ReactRouterOnly} />
   </>);
 }
 
---
+++ react/react-sample/src/page/ReactRouterOnly.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
+import { Route } from 'react-router';
+
+const ReactRouterOnly: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col sm={3} >
+            <Navbar className="flex-column" variant="dark" bg="dark">
+              <Navbar.Brand>React Router Only</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbar-nav-pills" />
+              <Navbar.Collapse id="navbar-nav-pills">
+                  <Nav className="flex-column" variant="pills">
+                  <Nav.Link href="/react-router-only/hoge1">Hoge1</Nav.Link>
+                  <Nav.Link href="/react-router-only/hoge2">Hoge2</Nav.Link>
+                  <Nav.Link href="/react-router-only/hoge3">Hoge3</Nav.Link>
+                  <Nav.Link href="/react-router-only/hoge4">Hoge4</Nav.Link>
+                  <Nav.Link href="/react-router-only/hoge5">Hoge5</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+          </Col>
+          <Col sm={8}>
+            <Route path="/react-router-only/hoge1" component={() => <h1>Hoge1</h1>} />
+            <Route path="/react-router-only/hoge2" component={() => <h1>Hoge2</h1>} />
+            <Route path="/react-router-only/hoge3" component={() => <h1>Hoge3</h1>} />
+            <Route path="/react-router-only/hoge4" component={() => <h1>Hoge4</h1>} />
+            <Route path="/react-router-only/hoge5" component={() => <h1>Hoge5</h1>} />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default ReactRouterOnly;
